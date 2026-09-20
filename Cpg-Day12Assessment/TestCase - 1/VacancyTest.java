41a741f7d361484dbdf2814f26f510a525a661a8
VRakshitha05 <vrakshitha85@gmail.com>
2026-09-20 11:33:42 +0530

    Update VacancyTest.java

--- Cpg-Day12Assessment/TestCase - 1/VacancyTest.java
+++ Cpg-Day12Assessment/TestCase - 1/VacancyTest.java
@@ -1,6 +1,4 @@
-
 package tests;
-
 
 import org.testng.annotations.Test;
 
@@ -19,18 +17,14 @@ public class VacancyTest extends BaseClass {
     @Test
     public void createVacancyTest() {
 
-        // Read login data from Excel
         String username = ExcelReader.getData("Sheet1", 1, 0);
         String password = ExcelReader.getData("Sheet1", 1, 1);
-
-        // Read vacancy data from Excel
         String vacancyName = ExcelReader.getData("Sheet1", 1, 2);
         String jobTitle = ExcelReader.getData("Sheet1", 1, 3);
         String vacancyDescription = ExcelReader.getData("Sheet1", 1, 4);
         String hiringManager = ExcelReader.getData("Sheet1", 1, 5);
         String positions = ExcelReader.getData("Sheet1", 1, 6);
 
-        // Login
         System.out.println("Step 1: Logging in");
 
         loginPage = new LoginPage(driver);
@@ -38,7 +32,6 @@ public class VacancyTest extends BaseClass {
 
         System.out.println("Login successful");
 
-        // Open Recruitment and Vacancies
         System.out.println("Step 2: Opening Recruitment and Vacancies");
 
         recruitmentPage = new RecruitmentPage(driver);
@@ -46,45 +39,38 @@ public class VacancyTest extends BaseClass {
 
         System.out.println("Recruitment and Vacancies opened");
 
-        // Click Add
         System.out.println("Step 3: Clicking Add button");
 
         recruitmentPage.clickAddVacancy();
 
         System.out.println("Add Vacancy page opened");
 
-        // Create Vacancy Page object
         vacancyPage = new VacancyPage(driver);
 
-        // Enter Vacancy Name
         System.out.println("Step 4: Entering vacancy name");
 
         vacancyPage.enterVacancyName(vacancyName);
 
         System.out.println("Vacancy name entered");
 
-        // Select Job Title
         System.out.println("Step 5: Selecting job title");
 
         vacancyPage.selectJobTitle(jobTitle);
 
         System.out.println("Job title selected");
 
-        // Enter Description
         System.out.println("Step 6: Entering description");
 
         vacancyPage.enterDescription(vacancyDescription);
 
         System.out.println("Description entered");
 
-        // Select Hiring Manager
         System.out.println("Step 7: Selecting hiring manager");
 
         vacancyPage.selectHiringManager(hiringManager);
 
         System.out.println("Hiring manager selected");
 
-        // Enter Number of Positions
         System.out.println("Step 8: Entering number of positions");
 
         vacancyPage.enterNumberOfPositions(positions);
@@ -93,13 +79,13 @@ public class VacancyTest extends BaseClass {
 
         System.out.println("Vacancy details entered successfully");
 
-        // Click Save
         System.out.println("Step 9: Clicking Save button");
 
         vacancyPage.clickSave();
 
         System.out.println("Save button clicked successfully");
 
-        System.out.println("Vacancy creation process completed successfully");
+        System.out.println(
+                "Vacancy creation process completed successfully");
     }
 }
